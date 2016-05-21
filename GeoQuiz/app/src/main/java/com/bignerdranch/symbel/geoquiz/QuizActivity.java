9581406237808c49bef138764e63f2551c7329cf
stefy83 <stefy83@gmail.com>
2016-05-21 19:53:21 +0200

    encapsulamos en un metodo el update de las question, para no repetir codigo, ya que se hacia en el metodo oncreate y en el listener del boton next.

--- GeoQuiz/app/src/main/java/com/bignerdranch/symbel/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/symbel/geoquiz/QuizActivity.java
@@ -26,14 +26,17 @@ public class QuizActivity extends AppCompatActivity {
 
     private int mCurrentIndex = 0;
 
+    private void updateQuestion(){
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
@@ -55,10 +58,10 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                int question = mQuestionBank[mCurrentIndex].getTextResId();
-                mQuestionTextView.setText(question);
+                updateQuestion();
             }
         });
+        updateQuestion();
     }
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
